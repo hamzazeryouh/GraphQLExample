145ffe1c4f79699aada296f1ddb648161418259f
Hamza Zeryouh <zeryouhbusiness@gmail.com>
2022-05-25 16:56:09 +0200

    add Service

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+using GraphQL.Data;
+using GraphQL.Data.Interfaces;
+using GraphQL.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ISuperheroRepository, SuperheroRepository>();
+builder.Services.AddScoped<ISuperpowerRepository, SuperpowerRepository>();
+builder.Services.AddScoped<IMovieRepository, MovieRepository>();
+// Add Application Db Context options
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(configuration.GetConnectionString("SqlServer")));
 
 var app = builder.Build();
 
